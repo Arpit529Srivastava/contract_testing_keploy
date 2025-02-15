912ff293d83d4e1fa56c8feb24522fb34205c137
arpit529srivastava <arpitsrivastava529@gmail.com>
2025-02-15 14:54:18 +0530

    added main
    Signed-off-by: arpit529srivastava <arpitsrivastava529@gmail.com>

---
+++ user-services/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Arpit529stivastava/user-services/database"
+	"github.com/Arpit529stivastava/user-services/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	db := database.InitDB()
+	defer db.Close()
+
+// setup the router
+	r := mux.NewRouter()
+	routes.RegisterUserRoutes(r,db)
+
+	// start the server 🧿
+	log.Println("User service running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
