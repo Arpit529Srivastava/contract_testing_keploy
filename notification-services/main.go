7651d8d00f9e9becbce63145196b18c39e467228
arpit529srivastava <arpitsrivastava529@gmail.com>
2025-02-17 02:17:47 +0530

    all service are working fine
    Signed-off-by: arpit529srivastava <arpitsrivastava529@gmail.com>

--- notification-services/main.go
+++ notification-services/main.go
@@ -1,26 +1,70 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
-
-	"github.com/Arpit529stivastava/notification-services/config"
-	"github.com/Arpit529stivastava/notification-services/routes"
-	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
 )
 
+
+type Order struct {
+	ID           string    `json:"id"`
+	UserEmail    string    `json:"user_email"`
+	Product      string    `json:"product"`
+	Quantity     int       `json:"quantity"`
+	Price        float64   `json:"price"`
+	CreatedAt    time.Time `json:"created_at"`
+	PaymentStatus string   `json:"payment_status"`
+}
+
 func main() {
-	// Connect to Database
-	config.ConnectDB()
+	go func (){
+		ticker := time.NewTicker(10 * time.Second)
+		defer ticker.Stop()
 
-	// Initialize Router
-	r := gin.Default()
+		for range ticker.C{
+			checkpaymentstatus()
+		}
+	}()
 
-	// Setup Routes
-	routes.SetupRoutes(r)
 
-	// Start Server
-	log.Println("Notification Service is running on port 8004")
-	if err := r.Run(":8004"); err != nil {
-		log.Fatal("Failed to start server:", err)
+	http.HandleFunc("/notify", func(w http.ResponseWriter, r *http.Request) {
+		checkpaymentstatus()
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]string {"message" : "Payment method triggered"})
+	})
+	fmt.Println("Notification services are running on localhost:8084 😚")
+	log.Fatal(http.ListenAndServe(":8084", nil))
+
+
+}
+
+
+func checkpaymentstatus() {
+	resp, err := http.Get("http://localhost:8081/orders")
+	if err != nil {
+		log.Println("Error fetching the order from the order database 😭😭")
+		return
 	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Println("Failed to fetch the orders ❌")
+		return
+	}
+	var orders []Order 
+	if err := json.NewDecoder(resp.Body).Decode(&orders); err != nil{
+		log.Println("Error Decoding the data 😵‍💫")
+		return
+	}
+
+	// checking for the completed payments
+
+	for _, check := range orders {
+		if check.PaymentStatus == "completed"{
+			log.Printf("Payment successful 👏🏻 for order ID: %s, User: %s, Product: %s\n", check.ID, check.UserEmail, check.Product)
+		}
+	}
+
 }
--- notification-services/models/notification.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Notification struct {
-	ID      int    `json:"id"`
-	UserID  int    `json:"user_id"`
-	Message string `json:"message"`
-	Status  string `json:"status"`
-}
